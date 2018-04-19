a34544cc4f818537303c53ef364a1e925814594c
zhongpeizhi <cdzhongpeizhi@jd.com>
2018-04-19 10:31:32 +0800

    update

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var http = require("http");
 var fs = require("fs");
 
 var url ='http://img20.360buyimg.com/babel/s380x300_jfs/t18115/116/1772264751/61591/9fd58275/5ad7172eNc632d061.jpg!q90!cc_190x150';
-
+var output='/Users/pacez/Desktop/2.jpg';
 http.get(url, function(res){
     var imgData = "";
     res.setEncoding("binary");
@@ -16,6 +16,6 @@ http.get(url, function(res){
       .fill(255, 255, 255,100)
       .draw(images(new Buffer(imgData,'binary')),10,10)
       .draw(images(new Buffer(imgData,'binary')),100,100)
-      .save('/Users/pacez/Desktop/2.jpg')
+      .save(output)
     });
 });
